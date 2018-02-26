7d7af38e1d7b1a6481bb46f19464a1c7227a7638
IamWaffle <ryanthecat17@gmail.com>
2018-02-26 13:15:06 -0500

    added base classes

---
+++ Project 1/src/Card/Deck.java
@@ -0,0 +1,43 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Deck.java
+ * Project name: project 1
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Shupe Ryan, shuper@etsu.edu
+ * Course: CSCI 1260
+ * Creation Date: Feb 26, 2018
+ * ---------------------------------------------------------------------------
+ */
+
+package Card;
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Feb 26, 2018
+ * <hr>
+ * 
+ * @author Ryan Shupe
+ */
+public class Deck
+{
+	private Card [ ]	deck;
+	private int			nextCard;
+	
+	/**
+	 * No Arg Constructor        
+	 *
+	 * <hr>
+	 * Date created: Feb 26, 2018 
+	 *
+	 * 
+	 */
+	public Deck ()
+	{
+		
+	}
+	
+	
+
+}
---
+++ Project 1/src/Card/Suit.java
@@ -0,0 +1,26 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Suit.java
+ * Project name: project 1
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Shupe Ryan, shuper@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Feb 26, 2018
+ * ---------------------------------------------------------------------------
+ */
+
+package Card;
+
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Feb 26, 2018
+ * <hr>
+ * @author Ryan Shupe
+ */
+public enum Suit
+{
+
+}
