6c21af4a9604b0721ee093ec9c5a0f91daefc2f0
IamWaffle <ryanthecat17@gmail.com>
2018-03-23 16:36:57 -0400

    + doctumentation

--- Project 2/src/addressBook/Contact.java
+++ Project 2/src/addressBook/Contact.java
@@ -27,28 +27,27 @@ import java.io.*;
 public class Contact
 {
 
-	ContactType			type;			//the Contact type
-	private String		name;			//String to hold the name
-	private String		streetAddress;  //String to hold the address
-	private String		city;			//String to hold the city
-	private String		state;			//String to hold the state
-	private String		zipCode;		//String to hold the zip code
-	private String		phone;			//String to hold the phone number (in format)
-	private String		email;			//String to hold the email (in format)
+	ContactType			type;										// the Contact type
+	private String		name;										// String to hold the name
+	private String		streetAddress;					  // String to hold the address
+	private String		city;										// String to hold the city
+	private String		state;										// String to hold the state
+	private String		zipCode;								// String to hold the zip code
+	private String		phone;										// String to hold the phone number (in format)
+	private String		email;										// String to hold the email (in format)
 
-	private String [ ]	statesArray	= new String [50];   //Array to hold the state abbreviations 
+	private String [ ]	statesArray	= new String [50];	   // Array to hold the state abbreviations
 
-	File				file;			//File object
-	Scanner				inputFile;		//Scanner that opens the file
+	File				file;										// File object
+	Scanner				inputFile;								// Scanner that opens the file
 
-	
 	/**
 	 * No Arg Constructor
 	 *
 	 * <hr>
 	 * Date created: Mar 18, 2018
 	 */
-	
+
 	public Contact ( )
 	{
 		setType ("OTHER");
@@ -60,6 +59,16 @@ public class Contact
 		setPhone ("0000000000");
 		setEmail ("temp@email.address");
 	}
+
+	/**
+	 * Arg Constructor
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param ContactType type, String name, String addr, String city, String state, String zip, String phone,
+	 *            String email
+	 */
 
 	public Contact (ContactType type, String name, String addr, String city, String state, String zip, String phone,
 					String email)
@@ -74,6 +83,15 @@ public class Contact
 		this.email = email;
 	}
 
+	/**
+	 * Copy Constructor
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param Contact origninal
+	 */
+
 	public Contact (Contact original)
 	{
 		setName (original.getName ( ));
@@ -84,6 +102,15 @@ public class Contact
 		setPhone (original.getPhone ( ));
 		setEmail (original.getEmail ( ));
 	}
+
+	/**
+	 * ToString - sends back the contact elements in a readable format
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return String output
+	 */
 
 	public String toString ( )
 	{
@@ -97,6 +124,13 @@ public class Contact
 						"\nEmail: " + getEmail ( );
 		return output;
 	}
+
+	/**
+	 * fillStates - fills the states array
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 */
 
 	public void fillStates ( )
 	{
@@ -120,6 +154,14 @@ public class Contact
 	}
 
 	// setters
+	/**
+	 * setType - sets the type of the contact from the contact type enum
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String typeString
+	 */
 	public void setType (String typeString)
 	{
 		try
@@ -132,20 +174,55 @@ public class Contact
 		}
 	}
 
+	/**
+	 * setName - sets the name of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String name
+	 */
 	public void setName (String name)
 	{
 		this.name = name;
 	}
+
+	/**
+	 * setAddr - sets the address of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String addr
+	 */
 
 	public void setAddr (String addr)
 	{
 		streetAddress = addr;
 	}
 
+	/**
+	 * setCity - sets the city of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String city
+	 */
+
 	public void setCity (String city)
 	{
 		this.city = city;
 	}
+
+	/**
+	 * setState - makes sure the state is valid, then sets the state of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String statePassed
+	 */
 
 	public void setState (String statePassed)
 	{
@@ -162,10 +239,28 @@ public class Contact
 		}
 	}
 
+	/**
+	 * setZip - sets the zip of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String zip
+	 */
+
 	public void setZip (String zip)
 	{
 		zipCode = zip;
 	}
+
+	/**
+	 * setPhone - sets the phone number of the contact the correct format
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String number
+	 */
 
 	public void setPhone (String number)
 	{
@@ -181,6 +276,14 @@ public class Contact
 
 	}
 
+	/**
+	 * setEmail - sets the email of the contact the correct format
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @param String input
+	 */
 	public void setEmail (String input)
 	{
 		if (input.indexOf ("@") >= 0 && input.indexOf ("@") < input.indexOf ("."))
@@ -194,40 +297,107 @@ public class Contact
 	}
 
 	// getters
+	/**
+	 * getType - returns the type of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return type.toString()
+	 */
 	public String getType ( )
 	{
 		return type.toString ( );
 	}
 
+	/**
+	 * getName - returns the Name of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return name
+	 */
 	public String getName ( )
 	{
 		return name;
 	}
+
+	/**
+	 * getAddr - returns the address of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return streetAddress
+	 */
 
 	public String getAddr ( )
 	{
 		return streetAddress;
 	}
 
+	/**
+	 * getCity - returns the city of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return city
+	 */
+
 	public String getCity ( )
 	{
 		return city;
 	}
 
+	/**
+	 * getState - returns the state of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return state
+	 */
 	public String getState ( )
 	{
 		return state;
 	}
 
+	/**
+	 * getZip - returns the zip code of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return zipCode
+	 */
 	public String getZip ( )
 	{
 		return zipCode;
 	}
 
+	/**
+	 * getPhone - returns the phone number of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return phone
+	 */
 	public String getPhone ( )
 	{
 		return phone;
 	}
+
+	/**
+	 * getEmail - returns the email address of the contact
+	 *
+	 * <hr>
+	 * Date created: Mar 18, 2018
+	 * 
+	 * @return email
+	 */
 
 	public String getEmail ( )
 	{
